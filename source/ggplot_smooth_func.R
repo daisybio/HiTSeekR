8e845d4f2e06a8cd02ec4b0f77f479b648f8037b
mlist <mlist@mpi-inf.mpg.de>
2016-04-16 20:48:02 +0200

    fixed issue with newest ggplot2 version

--- source/ggplot_smooth_func.R
+++ source/ggplot_smooth_func.R
@@ -1,91 +1,120 @@
-library(proto)
-
-stat_smooth_func <- function (mapping = NULL, data = NULL, geom = "smooth", position = "identity",
-                              method = "auto", formula = y ~ x, se = TRUE, n = 80, fullrange = FALSE,
-                              level = 0.95, na.rm = FALSE, ...) {
-  StatSmoothFunc$new(mapping = mapping, data = data, geom = geom, position = position,
-                     method = method, formula = formula, se = se, n = n, fullrange = fullrange,
-                     level = level, na.rm = na.rm, ...)
+stat_smooth_func <- function(mapping = NULL, data = NULL,
+                             geom = "smooth", position = "identity",
+                             ...,
+                             method = "auto",
+                             formula = y ~ x,
+                             se = TRUE,
+                             n = 80,
+                             span = 0.75,
+                             fullrange = FALSE,
+                             level = 0.95,
+                             method.args = list(),
+                             na.rm = FALSE,
+                             show.legend = NA,
+                             inherit.aes = TRUE,
+                             xpos = NULL,
+                             ypos = NULL) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = StatSmoothFunc,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      method = method,
+      formula = formula,
+      se = se,
+      n = n,
+      fullrange = fullrange,
+      level = level,
+      na.rm = na.rm,
+      method.args = method.args,
+      span = span,
+      xpos = xpos,
+      ypos = ypos,
+      ...
+    )
+  )
 }
 
-StatSmoothFunc <- proto(ggplot2:::Stat, {
-  objname <- "smooth"
-  
-  calculate_groups <- function(., data, scales, method="auto", formula=y~x, ...) {
-    rows <- daply(data, .(group), function(df) length(unique(df$x)))
-    
-    if (all(rows == 1) && length(rows) > 1) {
-      message("geom_smooth: Only one unique x value each group.",
-              "Maybe you want aes(group = 1)?")
-      return(data.frame())
-    }
-    
-    # Figure out what type of smoothing to do: loess for small datasets,
-    # gam with a cubic regression basis for large data
-    # This is based on the size of the _largest_ group.
-    if (identical(method, "auto")) {
-      groups <- count(data, "group")
-      
-      if (max(groups$freq) < 1000) {
-        method <- "loess"
-        message('geom_smooth: method="auto" and size of largest group is <1000,',
-                ' so using loess.',
-                ' Use \'method = x\' to change the smoothing method.')
-      } else {
-        method <- "gam"
-        formula <- y ~ s(x, bs = "cs")
-        message('geom_smooth: method="auto" and size of largest group is >=1000,',
-                ' so using gam with formula: y ~ s(x, bs = "cs").',
-                ' Use \'method = x\' to change the smoothing method.')
-      }
-    }
-    if (identical(method, "gam")) try_require("mgcv")
-    
-    .super$calculate_groups(., data, scales, method = method, formula = formula, ...)
-  }
-  
-  calculate <- function(., data, scales, method="auto", formula=y~x, se = TRUE, n=80, fullrange=FALSE, xseq = NULL, level=0.95, na.rm = FALSE, ...) {
-    data <- remove_missing(data, na.rm, c("x", "y"), name="stat_smooth")
-    if (length(unique(data$x)) < 2) {
-      # Not enough data to perform fit
-      return(data.frame())
-    }
-    
-    if (is.null(data$weight)) data$weight <- 1
-    
-    if (is.null(xseq)) {
-      if (is.integer(data$x)) {
-        if (fullrange) {
-          xseq <- scale_dimension(scales$x, c(0, 0))
-        } else {
-          xseq <- sort(unique(data$x))
-        }
-      } else {
-        if (fullrange) {
-          range <- scale_dimension(scales$x, c(0, 0))
-        } else {
-          range <- range(data$x, na.rm=TRUE)
-        }
-        xseq <- seq(range[1], range[2], length=n)
-      }
-    }
-    if (is.character(method)) method <- match.fun(method)
-    
-    method.special <- function(...)
-      method(formula, data=data, weights=weight, ...)
-    model <- safe.call(method.special, list(...), names(formals(method)))
-    
-    predictdf(model, xseq, se, level)
-    m = model
-    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                     list(a = format(coef(m)[1], digits = 3), 
-                          b = format(coef(m)[2], digits = 3), 
-                          r2 = format(summary(m)$r.squared, digits = 3)))
-    func_string = as.character(as.expression(eq))
-    
-    data.frame(x=min(data$x)*0.9, y=max(data$y)*0.9, label=func_string)
-  }
-  
-  required_aes <- c("x", "y")
-  default_geom <- function(.) GeomSmooth
-})
+
+StatSmoothFunc <- ggproto("StatSmooth", Stat,
+                          
+                          setup_params = function(data, params) {
+                            # Figure out what type of smoothing to do: loess for small datasets,
+                            # gam with a cubic regression basis for large data
+                            # This is based on the size of the _largest_ group.
+                            if (identical(params$method, "auto")) {
+                              max_group <- max(table(data$group))
+                              
+                              if (max_group < 1000) {
+                                params$method <- "loess"
+                              } else {
+                                params$method <- "gam"
+                                params$formula <- y ~ s(x, bs = "cs")
+                              }
+                            }
+                            if (identical(params$method, "gam")) {
+                              params$method <- mgcv::gam
+                            }
+                            
+                            params
+                          },
+                          
+                          compute_group = function(data, scales, method = "auto", formula = y~x,
+                                                   se = TRUE, n = 80, span = 0.75, fullrange = FALSE,
+                                                   xseq = NULL, level = 0.95, method.args = list(),
+                                                   na.rm = FALSE, xpos=NULL, ypos=NULL) {
+                            if (length(unique(data$x)) < 2) {
+                              # Not enough data to perform fit
+                              return(data.frame())
+                            }
+                            
+                            if (is.null(data$weight)) data$weight <- 1
+                            
+                            if (is.null(xseq)) {
+                              if (is.integer(data$x)) {
+                                if (fullrange) {
+                                  xseq <- scales$x$dimension()
+                                } else {
+                                  xseq <- sort(unique(data$x))
+                                }
+                              } else {
+                                if (fullrange) {
+                                  range <- scales$x$dimension()
+                                } else {
+                                  range <- range(data$x, na.rm = TRUE)
+                                }
+                                xseq <- seq(range[1], range[2], length.out = n)
+                              }
+                            }
+                            # Special case span because it's the most commonly used model argument
+                            if (identical(method, "loess")) {
+                              method.args$span <- span
+                            }
+                            
+                            if (is.character(method)) method <- match.fun(method)
+                            
+                            base.args <- list(quote(formula), data = quote(data), weights = quote(weight))
+                            model <- do.call(method, c(base.args, method.args))
+                            
+                            m = model
+                            eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                                             list(a = format(coef(m)[1], digits = 3), 
+                                                  b = format(coef(m)[2], digits = 3), 
+                                                  r2 = format(summary(m)$r.squared, digits = 3)))
+                            func_string = as.character(as.expression(eq))
+                            
+                            if(is.null(xpos)) xpos = min(data$x)*0.9
+                            if(is.null(ypos)) ypos = max(data$y)*0.9
+                            data.frame(x=xpos, y=ypos, label=func_string)
+                            
+                          },
+                          
+                          required_aes = c("x", "y")
+)
+
+
+
