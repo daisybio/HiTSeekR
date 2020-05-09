feb4f19c41c6be4e21381f713c54cca0a4f26c38
Quirin Manz <qu.manz@tum.de>
2020-05-09 21:50:55 +0200

    adding cookie consent banner

--- ui.R
+++ ui.R
@@ -104,7 +104,8 @@ elts <- list(
     }
                          ')),
     tags$link(rel = "stylesheet", type = "text/css", href = "HiTSeekR.css"),
-    shinyalert("general_status"), fluidRow(id='helpCheckBox', tags$style(type="text/css", "#helpCheckBox { margin-left:50px;}"), checkboxInput("showHelpText", "Show help", FALSE))
+    shinyalert("general_status"), fluidRow(id='helpCheckBox', tags$style(type="text/css", "#helpCheckBox { margin-left:50px;}"), checkboxInput("showHelpText", "Show help", FALSE)),
+    HTML('<link rel="stylesheet" type="text/css" href="cookieconsent.min.css"/><script src="cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.wpcc.init({"colors":{"popup":{"background":"#cff5ff","text":"#000000","border":"#5e99c2"},"button":{"background":"#5e99c2","text":"#ffffff"}}, "padding":"none","margin":"none","fontsize":"tiny","content":{"href":"https://www.learn-about-cookies.com/"},"position":"bottom-right"})});</script>')
   ),
   id="mainNavbar",  
   position="fixed-top",  
